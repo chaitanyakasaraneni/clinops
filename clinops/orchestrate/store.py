4c8a271d65fea6cefc33212fa17252a95e4d1992
chaitanyakasaraneni <24287691+chaitanyakasaraneni@users.noreply.github.com>
2026-04-08 13:23:40 -0500

    add: pre-commit checks to handle formatting errors

--- clinops/orchestrate/store.py
+++ clinops/orchestrate/store.py
@@ -174,9 +174,7 @@ class GCSPipelineStore:
                 ) from exc
 
             if self.credentials_path:
-                creds = service_account.Credentials.from_service_account_file(
-                    self.credentials_path
-                )
+                creds = service_account.Credentials.from_service_account_file(self.credentials_path)
                 self._client = storage.Client(credentials=creds)
             else:
                 self._client = storage.Client()
